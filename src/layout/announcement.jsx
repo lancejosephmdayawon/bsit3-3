01ebcda348e2ab80b94bb426f4bd12b148990147
lancejosephmdayawon <lancejosephmdayawon@gmail.com>
2026-03-07 12:22:20 +0800

    Added respect to new lines in title also in announcement

--- src/layout/announcement.jsx
+++ src/layout/announcement.jsx
@@ -2,7 +2,7 @@ export default function Announcement({ title, message, date }) {
   return (
     <div className="bg-white/40 backdrop-blur-md p-6 rounded-[20px] border border-white/50 hover:bg-white/50 transition-all shadow-sm">
       <div className="flex justify-between items-start mb-2">
-        <h3 className="text-xl font-bold text-[#8A6E5D] mr-2">{title}</h3>
+        <h3 className="text-xl font-bold text-[#8A6E5D] mr-2 whitespace-pre-line">{title}</h3>
         <span className="text-xs font-medium text-[#B5A996] bg-white/60 px-3 py-1 rounded-full">
           {date}
         </span>
